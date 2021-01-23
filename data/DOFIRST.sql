9a34886fa607df41d0f1a74ae46c91887e28fea4
Rich Davis <rdavis@ip-192-168-1-55.ec2.internal>
2021-01-23 12:47:54 -0500

    updates

--- data/DOFIRST.sql
+++ data/DOFIRST.sql
@@ -4,7 +4,7 @@
 
     start the docker container like this first:
 
-    docker run --rm -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD='pw' -v '/Users/rdavis/code/ag-grid-server-side-nodejs-example/data:/data' mysql:latest
+    docker run --rm -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD='pw' -v '/Users/rdavis/code/gpegrid-serverside/data:/setup' mysql:latest
 
     Make sure to wait for the container to be ready before you move on. You should see:
     
@@ -20,12 +20,26 @@
 
     the password is 'pw' set above. Now manually enter these commands:
 
-    source /data/DOFIRST.sql;
-    source /data/olympic_winners.sql;
+    source /setup/DOFIRST.sql;
+    source /setup/olympic_winners.sql;
+
+    BEFORE YOU QUIT MAKE SURE THE sample_data and olympic_winners tables exist;
+    
+    use sample_data;
+    desc olympic_winners;
+    select count(*) from olympic_winners;   -- and there are 8000+ records 
+    \q 
+
+    to get out my mysql
+
+    yarn install 
+    yarn start 
+
+    http://localhost:4000
 
  */
 CREATE DATABASE sample_data;
 CREATE USER 'reporting_app'@'%' IDENTIFIED WITH mysql_native_password BY 'password123';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,ALTER,DROP ON sample_data.* TO 'reporting_app'@'%';
 FLUSH PRIVILEGES;
-SOURCE /data/olympic_winners.sql;
+FLUSH TABLES;
