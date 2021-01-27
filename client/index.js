0a22bc03a07aa4e7f15d00bb618ae152692f0599
Rich Davis <vgcrld@gmail.com>
2021-01-27 15:34:38 -0500

    format number and set formatter

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-import {Grid} from 'ag-grid-community';
+import { Grid } from 'ag-grid-community';
 import 'ag-grid-enterprise';
 
 import "ag-grid-community/dist/styles/ag-grid.css";
@@ -46,46 +46,46 @@ const gridOptions = {
             editable: true,
         },
         groupColumn: {
-            rowGroup: true, 
+            rowGroup: true
         },
     },
 
     columnDefs: [
-        { type: [ 'showColumn', 'groupColumn' ], headerName: 'Activity', field: 'CfgTsmTimelineActivity' },
-        { type: [ 'showColumn', 'groupColumn' ], headerName: 'Entity', field: 'CfgTsmTimelineEntity' },
-        { type: [ 'showColumn', 'valueColumn' ], headerName: 'Cost', field: 'null'},
-        { type: [ 'showColumn', 'minColumn' ], headerName: 'Timestamp', field: 'poll_ts' },
-        { type: [ 'showColumn', 'sumColumn' ], headerName: 'Bytes', field: 'TSMTIMELINE_Bytes' },
-        { type: [ 'hideColumn', 'minColumn' ], headerName: 'Start', field: 'epoch_start' },
-        { type: [ 'hideColumn', 'minColumn' ], headerName: 'End', field: 'epoch_end' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Examined', field: 'TSMTIMELINE_Examined' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Affected', field: 'TSMTIMELINE_Affected' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Failed', field: 'TSMTIMELINE_Failed' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Idle', field: 'TSMTIMELINE_Idle' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Media Wait', field: 'TSMTIMELINE_Mediaw' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Processes', field: 'TSMTIMELINE_Processes' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Completion Code', field: 'TSMTIMELINE_Completion_code' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Comm Wait', field: 'TSMTIMELINE_Comm_wait' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Protected', field: 'TSMTIMELINE_Bytes_protected' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Written', field: 'TSMTIMELINE_Bytes_written' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Dedup Savings', field: 'TSMTIMELINE_Dedup_savings' },
-        { type: [ 'hideColumn', 'sumColumn' ], headerName: 'Compression Savings', field: 'TSMTIMELINE_Comp_savings' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Details', field: 'CfgTsmTimelineActivity_details' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Type', field: 'CfgTsmTimelineActivity_type' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Number', field: 'CfgTsmTimelineNumber' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'As Entity', field: 'CfgTsmTimelineAs_entity' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Sub Entity', field: 'CfgTsmTimelineSub_entity' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Comm Method', field: 'CfgTsmTimelineCommmeth' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Address', field: 'CfgTsmTimelineAddress' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Schedule Name', field: 'CfgTsmTimelineSchedule_name' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Successful', field: 'CfgTsmTimelineSuccessful' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Volumne', field: 'CfgTsmTimelineVolume_name' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Drive', field: 'CfgTsmTimelineDrive_name' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Library', field: 'CfgTsmTimelineLibrary_name' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Last Use', field: 'CfgTsmTimelineLast_use' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Offsite Volumes', field: 'CfgTsmTimelineNum_offsite_vols' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Instance', field: 'CfgTsmTimelineInstance' },
-        { type: [ 'hideColumn', 'maxColumn' ], headerName: 'Node', field: 'CfgTsmTimelineNodeName' }
+        { type: ['showColumn', 'groupColumn'], headerName: 'Activity', field: 'CfgTsmTimelineActivity' },
+        { type: ['showColumn', 'groupColumn'], headerName: 'Entity', field: 'CfgTsmTimelineEntity' },
+        { type: ['showColumn', 'valueColumn'], headerName: 'Cost', field: 'null' },
+        { type: ['showColumn', 'minColumn', 'numericColumn'], headerName: 'Timestamp', field: 'poll_ts' },
+        { type: ['showColumn', 'sumColumn', 'numericColumn'], headerName: 'Bytes', field: 'TSMTIMELINE_Bytes', valueFormatter: commaSeparateNumber },
+        { type: ['hideColumn', 'minColumn', 'numericColumn'], headerName: 'Start', field: 'epoch_start' },
+        { type: ['hideColumn', 'minColumn', 'numericColumn'], headerName: 'End', field: 'epoch_end' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Examined', field: 'TSMTIMELINE_Examined' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Affected', field: 'TSMTIMELINE_Affected' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Failed', field: 'TSMTIMELINE_Failed' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Idle', field: 'TSMTIMELINE_Idle' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Media Wait', field: 'TSMTIMELINE_Mediaw' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Processes', field: 'TSMTIMELINE_Processes' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Completion Code', field: 'TSMTIMELINE_Completion_code' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Comm Wait', field: 'TSMTIMELINE_Comm_wait' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Protected', field: 'TSMTIMELINE_Bytes_protected' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Written', field: 'TSMTIMELINE_Bytes_written' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Dedup Savings', field: 'TSMTIMELINE_Dedup_savings' },
+        { type: ['hideColumn', 'sumColumn', 'numericColumn'], headerName: 'Compression Savings', field: 'TSMTIMELINE_Comp_savings' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Details', field: 'CfgTsmTimelineActivity_details' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Type', field: 'CfgTsmTimelineActivity_type' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Number', field: 'CfgTsmTimelineNumber' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'As Entity', field: 'CfgTsmTimelineAs_entity' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Sub Entity', field: 'CfgTsmTimelineSub_entity' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Comm Method', field: 'CfgTsmTimelineCommmeth' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Address', field: 'CfgTsmTimelineAddress' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Schedule Name', field: 'CfgTsmTimelineSchedule_name' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Successful', field: 'CfgTsmTimelineSuccessful' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Volumne', field: 'CfgTsmTimelineVolume_name' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Drive', field: 'CfgTsmTimelineDrive_name' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Library', field: 'CfgTsmTimelineLibrary_name' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Last Use', field: 'CfgTsmTimelineLast_use' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Offsite Volumes', field: 'CfgTsmTimelineNum_offsite_vols' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Instance', field: 'CfgTsmTimelineInstance' },
+        { type: ['hideColumn', 'maxColumn', 'numericColumn'], headerName: 'Node', field: 'CfgTsmTimelineNodeName' }
     ]
 
     // debug: true,
@@ -101,22 +101,30 @@ new Grid(gridDiv, gridOptions);
 
 const datasource = {
     getRows(params) {
-         console.log(JSON.stringify(params.request, null, 1));
+        console.log(JSON.stringify(params.request, null, 1));
 
-         fetch('./gpeTsmTimeline/', {
-             method: 'post',
-             body: JSON.stringify(params.request),
-             headers: {"Content-Type": "application/json; charset=utf-8"}
-         })
-         .then(httpResponse => httpResponse.json())
-         .then(response => {
-             params.successCallback(response.rows, response.lastRow);
-         })
-         .catch(error => {
-             console.error(error);
-             params.failCallback();
-         })
+        fetch('./gpeTsmTimeline/', {
+            method: 'post',
+            body: JSON.stringify(params.request),
+            headers: { "Content-Type": "application/json; charset=utf-8" }
+        })
+            .then(httpResponse => httpResponse.json())
+            .then(response => {
+                params.successCallback(response.rows, response.lastRow);
+            })
+            .catch(error => {
+                console.error(error);
+                params.failCallback();
+            })
     }
 };
 
 gridOptions.api.setServerSideDatasource(datasource);
+
+function commaSeparateNumber(params){
+    let val = params.value;
+    while (/(\d+)(\d{3})/.test(val.toString())){
+      val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+    }
+    return val;
+  }
