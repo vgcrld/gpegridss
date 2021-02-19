9020422a21b010791d0aa9c4b48d6ccb40dba383
Rich Davis <rdavis@ip-192-168-1-55.ec2.internal>
2021-02-19 10:42:45 -0500

    adjust client side for items or others

--- client/index.js
+++ client/index.js
@@ -4,82 +4,20 @@ import 'ag-grid-enterprise';
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-const gridOptions = {
+const tsmColumnDefs = [
+    { headerName: 'Activity', field: 'CfgTsmTimelineActivity', rowGroup: true },
+    { headerName: 'Year', field: 'toYear(toStartOfYear(poll_ts))', rowGroup: true },
+    { headerName: 'Month', field: 'toMonth(toStartOfMonth(poll_ts))', rowGroup: true },
+    { headerName: 'Day', field: 'toDayOfMonth(toStartOfDay(poll_ts))', rowGroup: true },
+    { headerName: 'Entity', field: 'CfgTsmTimelineEntity', aggFunc: 'max' },
+    { headerName: 'Bytes', field: 'TSMTIMELINE_Examined', aggFunc: 'max', valueFormatter: commaSeparateNumber },
+    { headerName: 'Bytes', field: 'TSMTIMELINE_Affected', aggFunc: 'max', valueFormatter: commaSeparateNumber },
+    { headerName: 'Bytes', field: 'TSMTIMELINE_Failed', aggFunc: 'max', valueFormatter: commaSeparateNumber },
+    { headerName: 'Bytes', field: 'TSMTIMELINE_Idle', aggFunc: 'max', valueFormatter: commaSeparateNumber }
+];
 
-    rowModelType: 'serverSide',
-    serverSideStoreType: 'full',
-    animateRows: true,
-    enableCharts: true,
-    grouping: true,
-    enableRangeSelection: true,
-    sideBar: true,   
-    groupMultiAutoColumn: false,
-    enablePivotMode: true, 
-    groupSelectsChildren: true,
-    groupUseEntireRow: false,
-    showOpenedGroup: false,
-
-
-    defaultColDef: {
-        filter: true,
-        sortable: true,
-        resizable: true
-    },
-
-    columnDefs: [
-        { headerName: 'Activity', field: 'CfgTsmTimelineActivity', rowGroup: true },
-        { headerName: 'Year', field: 'toYear(toStartOfYear(poll_ts))', rowGroup: true },
-        { headerName: 'Month', field: 'toMonth(toStartOfMonth(poll_ts))', rowGroup: true },
-        { headerName: 'Day', field: 'toDayOfMonth(toStartOfDay(poll_ts))', rowGroup: true },
-        { headerName: 'Entity', field: 'CfgTsmTimelineEntity', aggFunc: 'max' },
-        { headerName: 'Bytes', field: 'TSMTIMELINE_Bytes', aggFunc: 'max', valueFormatter: commaSeparateNumber }
-        // { field: 'TSMTIMELINE_Examined' },
-        // { field: 'TSMTIMELINE_Affected' },
-        // { field: 'TSMTIMELINE_Failed' },
-        // { field: 'TSMTIMELINE_Idle' },
-        // { field: 'null' },
-        // { field: 'epoch_start' },
-        // { field: 'epoch_end' },
-        // { field: 'TSMTIMELINE_Mediaw' },
-        // { field: 'TSMTIMELINE_Processes' },
-        // { field: 'TSMTIMELINE_Completion_code' },
-        // { field: 'TSMTIMELINE_Comm_wait' },
-        // { field: 'TSMTIMELINE_Bytes_protected' },
-        // { field: 'TSMTIMELINE_Bytes_written' },
-        // { field: 'TSMTIMELINE_Dedup_savings' },
-        // { field: 'TSMTIMELINE_Comp_savings' },
-        // { field: 'CfgTsmTimelineActivity_details' },
-        // { field: 'CfgTsmTimelineActivity_type' },
-        // { field: 'CfgTsmTimelineNumber' },
-        // { field: 'CfgTsmTimelineAs' },
-        // { field: 'CfgTsmTimelineSub_entity' },
-        // { field: 'CfgTsmTimelineCommmeth' },
-        // { field: 'CfgTsmTimelineAddress' },
-        // { field: 'CfgTsmTimelineSchedule_name' },
-        // { field: 'CfgTsmTimelineSuccessful' },
-        // { field: 'CfgTsmTimelineVolume_name' },
-        // { field: 'CfgTsmTimelineDrive_name' },
-        // { field: 'CfgTsmTimelineLibrary_name' },
-        // { field: 'CfgTsmTimelineLast_use' },
-        // { field: 'CfgTsmTimelineNum_offsite_vols' },
-        // { field: 'CfgTsmTimelineInstance' },
-        // { field: 'CfgTsmTimelineNodeName' }
-    ],
-
-    debug: true,
-
-    cacheBlockSize: 3000,
-    // maxBlocksInCache: 3,
-    // purgeClosedRowNodes: true,
-    // maxConcurrentDatasourceRequests: 2,
-    // blockLoadDebounceMillis: 1000
-
-};
-
-const gridDiv = document.querySelector('#myGrid');
-new Grid(gridDiv, gridOptions);
-
-const datasource = {
+const tsmDatasource = {
+    
     getRows(params) {
         console.log(JSON.stringify(params.request, null, 1));
 
@@ -99,7 +37,45 @@ const datasource = {
     }
 };
 
-gridOptions.api.setServerSideDatasource(datasource);
+var gpeDatasource = tsmDatasource; 
+
+const gridOptions = {
+
+    rowModelType: 'serverSide',
+    serverSideStoreType: 'full',
+    animateRows: true,
+    enableCharts: true,
+    grouping: true,
+    enableRangeSelection: true,
+    sideBar: true,   
+    groupMultiAutoColumn: false,
+    enablePivotMode: true, 
+    groupSelectsChildren: true,
+    groupUseEntireRow: false,
+    showOpenedGroup: false,
+
+    columnDefs: tsmColumnDefs, 
+
+    defaultColDef: {
+        filter: true,
+        sortable: true,
+        resizable: true
+    },
+
+    debug: true,
+
+    cacheBlockSize: 3000,
+    // maxBlocksInCache: 3,
+    // purgeClosedRowNodes: true,
+    // maxConcurrentDatasourceRequests: 2,
+    // blockLoadDebounceMillis: 1000
+
+};
+
+const gridDiv = document.querySelector('#myGrid');
+new Grid(gridDiv, gridOptions);
+
+gridOptions.api.setServerSideDatasource(gpeDatasource);
 
 function commaSeparateNumber(params){
     let val = params.value;
